968f454a376d65b88168bdd7882137dbc5c40005
kdan80 <kierandansey80@gmail.com>
2022-11-04 15:46:45 +0000

    Main app component layout styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,15 +2,27 @@ import React from 'react'
 import DocumentHeader from 'components/DocumentHeader'
 
 type Props = {
+    location?: string
     children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ children, location }: Props) => {
+    const isHome = location === '/'
+    const is404 = location === '404'
+    const [isLoading, setIsLoading] = React.useState<boolean>(isHome)
+
     return (
-        <div className=''>
+        <div className='flex justify-center items-center min-h-screen border-2'>
             <DocumentHeader />
             {/* Background image for the entire site */}
-            <div className='bg-img w-screen h-screen bg-no-repeat bg-cover bg-fixed' />
+
+            <div className='fixed top-0 left-0 -z-10 bg-img w-screen h-screen bg-no-repeat bg-cover bg-fixed' />
+
+            {isLoading && isHome ? (
+                <div className='text-red-600'>Hello</div>
+            ) : (
+                <div className='text-red-600'>Goodbye</div>
+            )}
         </div>
     )
 }
