d48ac447eeab2b24ad65cfdad2dcd1120fdf75d5
kdan80 <kierandansey80@gmail.com>
2022-11-07 16:20:34 +0000

    Moved animations to framer-motion

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,57 +4,47 @@ import Nav from './Nav'
 import Hamburger from './Hamburger'
 import DropdownNav from './DropdownNav'
 import config from 'config/config'
-import { motion } from 'framer-motion'
+import { motion, useAnimationControls } from 'framer-motion'
 import useScrollDirection from 'hooks/useScrollDirection'
-
-// Framer motion animations
-const headerVariants = {
-    initial: {
-        translateY: 0,
-    },
-    show: {
-        translateY: 0,
-        backdropBlur: '16px',
-    },
-    hide: {
-        translateY: '-100%',
-    },
-}
-
-const headerTransition = {
-    ease: 'easeInOut',
-    bounce: 0,
-    duration: 0.3,
-}
 
 const Header = () => {
     const [dropdownIsOpen, setDropdownIsOpen] = React.useState<boolean>(false)
-    const [showHeader, setShowHeader] = React.useState<boolean>(true)
     const { scrollDirection, scrollY } = useScrollDirection()
-    const [blurHeader, setBlurHeader] = React.useState<boolean>(false)
+    const headerControls = useAnimationControls()
 
+    // This effect hook controls the header show/hide and blur animations
     React.useEffect(() => {
         if (scrollY >= 250 && scrollDirection === 'down') {
-            setShowHeader(false)
+            headerControls.start({
+                translateY: '-100%',
+            })
         } else {
-            setShowHeader(true)
+            headerControls.start({
+                translateY: 0,
+            })
         }
         if (scrollY === 0) {
-            setBlurHeader(false)
+            headerControls.start({
+                boxShadow: '0 2px 16px rgba(0, 0, 0, 0)',
+                backdropFilter: 'blur(0px)',
+            })
         }
         if (scrollY > 250 && scrollDirection === 'up') {
-            setBlurHeader(true)
+            headerControls.start({
+                boxShadow: '0 2px 16px rgba(0, 0, 0, 0.4)',
+                backdropFilter: 'blur(16px)',
+            })
         }
-    }, [scrollDirection, scrollY])
+    }, [scrollDirection, scrollY, headerControls])
 
     return (
         <motion.header
             // prettier-ignore
-            className={`fixed top-0 h-20 w-full py-0 px-[5vw] md:px-12 flex justify-between items-center text-lg transition duration-500 ease-in-out ${blurHeader ? 'backdrop-blur-lg shadow-[0_2px_16px_0_rgba(0,0,0,.4)]' : ''} `}
-            variants={headerVariants}
-            initial='initial'
-            animate={showHeader ? 'show' : 'hide'}
-            transition={headerTransition}>
+            className={`fixed top-0 h-20 w-full py-0 px-[5vw] md:px-12 flex justify-between items-center text-lg`}
+            animate={headerControls}
+            transition={{
+                duration: 0.4,
+            }}>
             <Link href='#landing'>
                 <div>
                     <span className='text-hero'>
