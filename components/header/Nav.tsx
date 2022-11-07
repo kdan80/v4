dc44bd9ee4f80fda9b78a39cae56bd6e322fc2f0
kdan80 <kierandansey80@gmail.com>
2022-11-07 15:02:58 +0000

    Header animations

--- components/header/Nav.tsx
+++ components/header/Nav.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 type NavLink = {
     name: string
@@ -10,20 +11,51 @@ interface Props {
     navLinks: NavLink[]
 }
 
+const navLinksListVariants = {
+    initial: { opacity: 0 },
+    animate: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+}
+
+const navLinksItemVariants = {
+    initial: {
+        opacity: 0,
+        translateY: '-100%',
+    },
+    animate: {
+        opacity: 1,
+        translateY: 0,
+        transition: {
+            duration: 0.3,
+        },
+    },
+}
+
 const Nav = ({ navLinks }: Props) => {
     return (
         <nav>
-            <ol className='hidden md:flex gap-8'>
+            <motion.ol
+                variants={navLinksListVariants}
+                initial='initial'
+                animate='animate'
+                className='hidden md:flex gap-8'>
                 {navLinks.map(({ name, url }, index) => (
-                    <li
+                    <motion.li
+                        variants={navLinksItemVariants}
                         className='hover:text-hero transition-all duration-500 ease-in-out'
                         key={index}>
-                        <Link className='' href={url}>
+                        <Link
+                            className=''
+                            href={url}>
                             {name}
                         </Link>
-                    </li>
+                    </motion.li>
                 ))}
-            </ol>
+            </motion.ol>
         </nav>
     )
 }
