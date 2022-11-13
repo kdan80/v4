3c26e945d6594dce1a17d07495a00394ff47cae3
kdan80 <kierandansey80@gmail.com>
2022-11-13 21:39:18 +0000

    Backdrop blur bug

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Link from 'next/link'
 import Nav from './Nav'
 import Hamburger from './Hamburger'
 import DropdownNav from './DropdownNav'
 import config from 'config/config'
-import { motion, useAnimationControls } from 'framer-motion'
+import { motion } from 'framer-motion'
 import useScrollDirection from 'hooks/useScrollDirection'
 
 // Framer animations for home link, should be the same as hamburgerVariants in Hamburger.tsx
@@ -27,41 +26,14 @@ interface Props {
 const Header = ({ scrolledToTop }: Props) => {
     const [dropdownIsOpen, setDropdownIsOpen] = React.useState<boolean>(false)
     const scrollDirection = useScrollDirection()
-    const headerControls = useAnimationControls()
-
-    // This effect hook controls the header show/hide and blur animations
-    React.useEffect(() => {
-        if (scrollDirection === 'down') {
-            headerControls.start({
-                translateY: '-100%',
-            })
-        } else {
-            headerControls.start({
-                translateY: 0,
-            })
-        }
-        if (scrolledToTop) {
-            headerControls.start({
-                boxShadow: '0 2px 16px rgba(0, 0, 0, 0)',
-                backdropFilter: 'blur(0px)',
-            })
-        }
-        if (!scrolledToTop && scrollDirection === 'up') {
-            headerControls.start({
-                boxShadow: '0 2px 16px rgba(0, 0, 0, 0.4)',
-                backdropFilter: 'blur(16px)',
-            })
-        }
-    }, [scrollDirection, headerControls, scrolledToTop])
 
     return (
-        <motion.header
+        <header
             // prettier-ignore
-            className={`fixed top-0 h-20 w-full py-0 px-[5vw] md:px-12 flex justify-between items-center text-lg z-20`}
-            animate={headerControls}
-            transition={{
-                duration: 0.4,
-            }}
+            className={`fixed top-0 h-20 w-full py-0 px-[5vw] md:px-12 flex justify-between items-center text-lg z-20 transition duration-500 ease-in-out
+            ${scrolledToTop ? '' : 'backdrop-blur drop-shadow-[0_2px_16px_rgba(0,0,0,0.4)]'}
+            ${scrollDirection === 'down' ? '-translate-y-full' : 'translate-y-0'} 
+            `}
         >
             <motion.div
                 variants={homeLinkVariants}
@@ -88,8 +60,103 @@ const Header = ({ scrolledToTop }: Props) => {
                 dropdownIsOpen={dropdownIsOpen}
                 setDropdownIsOpen={setDropdownIsOpen}
             />
-        </motion.header>
+        </header>
     )
 }
 
 export default Header
+
+// import React from 'react'
+// import Nav from './Nav'
+// import Hamburger from './Hamburger'
+// import DropdownNav from './DropdownNav'
+// import config from 'config/config'
+// import { motion, useAnimationControls } from 'framer-motion'
+// import useScrollDirection from 'hooks/useScrollDirection'
+
+// // Framer animations for home link, should be the same as hamburgerVariants in Hamburger.tsx
+// const homeLinkVariants = {
+//     initial: { opacity: 0 },
+//     animate: {
+//         opacity: 1,
+//         transition: {
+//             duration: 0.3,
+//             delay: 1,
+//             ease: 'easeInOut',
+//         },
+//     },
+// }
+
+// interface Props {
+//     scrolledToTop: boolean
+// }
+
+// const Header = ({ scrolledToTop }: Props) => {
+//     const [dropdownIsOpen, setDropdownIsOpen] = React.useState<boolean>(false)
+//     const scrollDirection = useScrollDirection()
+//     const headerControls = useAnimationControls()
+
+//     // This effect hook controls the header show/hide and blur animations
+//     React.useEffect(() => {
+//         if (scrollDirection === 'down') {
+//             headerControls.start({
+//                 translateY: '-100%',
+//             })
+//         } else {
+//             headerControls.start({
+//                 translateY: 0,
+//             })
+//         }
+//         if (scrolledToTop) {
+//             headerControls.start({
+//                 boxShadow: '0 2px 16px rgba(0, 0, 0, 0)',
+//                 backdropFilter: 'blur(0px)',
+//             })
+//         }
+//         if (!scrolledToTop && scrollDirection === 'up') {
+//             headerControls.start({
+//                 boxShadow: '0 2px 16px rgba(0, 0, 0, 0.4)',
+//                 backdropFilter: 'blur(16px)',
+//             })
+//         }
+//     }, [scrollDirection, headerControls, scrolledToTop])
+
+//     return (
+//         <motion.header
+//             // prettier-ignore
+//             className={`fixed top-0 h-20 w-full py-0 px-[5vw] md:px-12 flex justify-between items-center text-lg z-20`}
+//             animate={headerControls}
+//             transition={{
+//                 duration: 0.4,
+//             }}
+//         >
+//             <motion.div
+//                 variants={homeLinkVariants}
+//                 initial='initial'
+//                 animate='animate'
+//             >
+//                 <a href='#landing'>
+//                     <span className='text-hero'>
+//                         &lt;/&gt;{' '}
+//                         <span className='text-light-300 md:text-light-200 hover:text-hero transition ease-in-out duration-500'>
+//                             kieran dansey
+//                         </span>
+//                     </span>
+//                 </a>
+//             </motion.div>
+
+//             <Nav navLinks={config.navLinks} />
+//             <DropdownNav
+//                 navLinks={config.navLinks}
+//                 dropdownIsOpen={dropdownIsOpen}
+//                 setDropdownIsOpen={setDropdownIsOpen}
+//             />
+//             <Hamburger
+//                 dropdownIsOpen={dropdownIsOpen}
+//                 setDropdownIsOpen={setDropdownIsOpen}
+//             />
+//         </motion.header>
+//     )
+// }
+
+// export default Header
